6cdad928f04683b7bde64b4d56a7d477c72e5852
NEFUstudent <study.svfu@gmail.com>
2019-04-18 22:50:35 +0900

    pplab 9.2

---
+++ pplab9.2/pplab9.2/money.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "pch.h"
+
+class money
+{
+private:
+	int rubley;
+	unsigned char kopeek;
+public:
+	void set(int newRubley, int newKopeek);
+	long getRubley() { return rubley; };
+	unsigned char getKopeek() { return kopeek; };
+	money operator+(money& plusTime);
+	money operator-(money& plusTime);
+	money operator*(const int& plusTime);
+	void print();
+};
