bd2c0a0292b895040580c8a42f879ded7ad1a46b
NEFUstudent <study.svfu@gmail.com>
2019-05-02 17:33:32 +0900

    egg dll

---
+++ pplab11/egg/egg/egg.cpp
@@ -0,0 +1,20 @@
+﻿// egg.cpp : Определяет экспортированные функции для приложения DLL.
+//
+
+#include "stdafx.h"
+#include "egg.h"
+#include <iostream>
+#include <string>
+
+bool egg::broke(std::string who)
+{
+	say(who + " try to break to me!");
+	if(who!="mouse")
+		return false;
+	else return true;
+}
+
+void egg::say(std::string text)
+{
+	std::cout << text << std::endl;
+}
---
+++ pplab11/egg/egg/egg.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "stdafx.h"
+#include <iostream>
+
+#ifdef MAKEDLL
+#  define EXPORT __declspec(dllexport)
+#else
+#  define EXPORT __declspec(dllimport)
+#endif
+
+class egg {
+public:
+	int size;
+	int weight;
+	EXPORT bool broke(std::string who);
+	EXPORT void say(std::string text);
+};
