a34d449a4018e1bad9fe7688da7276b78d312e0c
NEFUstudent <study.svfu@gmail.com>
2019-06-01 10:51:02 +0900

    lab 15 pp

---
+++ pplab15/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result;
+            if (Int32.TryParse(textBox1.Text, out result))
+            {
+                MessageBox.Show("Возведено в степень");
+                result *= result;
+                textBox2.Text = System.Convert.ToString(result);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
